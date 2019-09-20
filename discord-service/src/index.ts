a762aa9141ab3ac8bcc0637c49123d5975609de9
Salepate <salepate@gmail.com>
2019-09-20 22:23:44 +0200

    use new bot constructor

--- discord-service/src/index.ts
+++ discord-service/src/index.ts
@@ -1,7 +1,7 @@
 import BotClient from "./bot/bot-client";
 import storyCommand from "./commands/story";
 
-let bot = new BotClient('INSERT_DISCORD_BOT_TOKEN_HERE')
+let bot = new BotClient(__dirname, 'INSERT_DISCORD_BOT_TOKEN_HERE')
 
 bot.run()
 bot.registerCommand(storyCommand)
